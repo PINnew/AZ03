ff8830ff5c40c861ff9331a37ed8025611c2483b
PINnew <pinnew@ya.ru>
2024-10-25 11:58:47 +0400

    add code new

--- dz6.py
+++ dz6.py
@@ -1,33 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# Инициализация драйвера
+
+# Инициализация браузера
 driver = webdriver.Chrome()
 
-# URL страницы
 url = 'https://www.divan.ru/category/divany-i-kresla'
 driver.get(url)
+time.sleep(5) # Задаём время, подгружаем сайт
+# Поиск всех светильников на странице
+elements = driver.find_elements(By.CSS_SELECTOR, '[data-testid="product-card"]')
+prices = []
 
-try:
-    # Ожидание появления элементов с ценами
-    elements = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME, 'ui-LD-ZU KIkOH'))
-    )
+for element in elements:
+    price = element.find_element(By.CSS_SELECTOR, '[itemprop="price"]').get_attribute('content')
+    price_text = price.replace('₽', '').replace(' ', '')  # Убираем символы и пробелы
+    if price_text.isdigit():  # Проверка, что цена корректная
+        prices.append(int(price_text))
 
-    # Парсинг цен
-    prices = []
-    for element in elements:
-        price_text = element.text.replace('₽', '').replace(' ', '')  # Убираем символы и пробелы
-        if price_text.isdigit():  # Проверка, что цена корректная
-            prices.append(int(price_text))
-
-finally:
-    # Закрываем драйвер
-    driver.quit()
+# Закрываем драйвер
+driver.quit()
 
 # Сохранение в CSV файл
 df = pd.DataFrame(prices, columns=['Price'])
